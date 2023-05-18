fc665cf20825c6feace8c5293ee002c3e8268d39
5WCP <oldman22660@gmail.com>
2023-05-19 00:26:51 +0800

    third commit

--- javascript/memPage.js
+++ javascript/memPage.js
@@ -3,6 +3,28 @@ if(!sessionStorage.getItem("userId")) {
     window.location.href = "../logInPage/logInPage.html";
 }
 
+const dropB = document.querySelector(".dropBtn");
+const dropDC = document.querySelector(".dropDownContent");
+const logOut = document.querySelector("#logOutB");
+
+dropB.addEventListener("click", function() {
+    dropDC.classList.toggle("show");
+})
+
+window.addEventListener("click", function(event) {
+    if (!event.target.matches(".dropBtn")) {
+        if (dropDC.classList.contains("show")) {
+            dropDC.classList.remove("show");
+        }
+    }
+});
+
+logOut.addEventListener("click", function() {
+    sessionStorage.removeItem("userId");
+    alert("已登出，進入登入頁面")
+    window.location.href = "../logInPage/logInPage.html";
+})
+
 // 帶出用戶名販賣的商品
 
 const revProId = document.querySelector("#reviseProId");
@@ -27,18 +49,26 @@ fetch("http://localhost:8080/find_mem_sell", {
         mge.innerHTML = "\u00A0" + `${checkData.message}` + "\u00A0"; 
     }
 
+    const seleProId = [SCProId, revProId, deleProId, CSProId];
+
     if(checkData.pro_id_list) {
         checkData.pro_id_list.forEach(function(i) {
-            const revProIdOp = document.createElement('option');
-            revProIdOp.value = i;
-            revProIdOp.innerText = i;
-            revProId.appendChild(revProIdOp);
+            seleProId.forEach((proId) => {
+                const op = document.createElement('option');
+                op.value = i;
+                op.innerText = i;
+                proId.appendChild(op);
+            });
         })
     }
 })
 .catch(function(error) {
     console.log(error)
 })
+
+// 取消訂單(賣)
+
+const SCProId = document.querySelector("#SCancelProId");
 
 // 新增商品
 
@@ -114,7 +144,10 @@ addProB.addEventListener("click", function() {
 
 const revPId = document.querySelector("#reviseProId")
 
-revPId.addEventListener("click", function() {
+revPId.addEventListener("change", function() {
+    revS1.value = null;
+    revS2.value = null;
+    revS3.value = null;
     let body = {
         "product_id": revProId.value
     }
@@ -131,18 +164,20 @@ revPId.addEventListener("click", function() {
     })
     .then(function(data) {
         const checkData = JSON.parse(JSON.stringify(data))
-        revProN.value = checkData.product_name
-        revProPr.value = checkData.price
-        revProS.value = checkData.stock
-        revPI.src = checkData.product_picture
-        if(checkData.sort_name[0]) {
-            revS1.value = checkData.sort_name[0]
-        }
-        if(checkData.sort_name[1]) {
-            revS2.value = checkData.sort_name[1]
-        }
-        if(checkData.sort_name[2]) {
-            revS3.value = checkData.sort_name[2]
+        if(checkData) {
+            revProN.value = checkData.product_name
+            revProPr.value = checkData.price
+            revProS.value = checkData.stock
+            revPI.src = checkData.product_picture
+            if(checkData.sort_name[0]) {
+                revS1.value = checkData.sort_name[0]
+            }
+            if(checkData.sort_name[1]) {
+                revS2.value = checkData.sort_name[1]
+            }
+            if(checkData.sort_name[2]) {
+                revS3.value = checkData.sort_name[2]
+            }
         }
     })
     .catch(function(error) {
@@ -162,6 +197,20 @@ const revS2 = document.querySelector("#revSort2");
 const revS3 = document.querySelector("#revSort3");
 const revPI = document.querySelector(".revProImg");
 let revPP;
+
+// 給出下拉式選單種類的值
+
+const sortsName = ["電子", "貴重品", "生活用品", "其他", "清潔用品"];
+const sortSelect = [addS1, addS2, addS3, revS1, revS2, revS3];
+
+sortsName.forEach((i) => {
+    sortSelect.forEach((sort) => {
+        const op = document.createElement('option');
+        op.value = i;
+        op.innerText = i;
+        sort.appendChild(op);
+    });
+});
 
 revProP.addEventListener("change", (e) => {
     const reader = new FileReader();
@@ -191,12 +240,15 @@ revProB.addEventListener("click", function() {
         "product_info": {
             "productId": revPId.value,
             "productName": revProN.value,
-            "productPicture": revPP,
             "stock": revProS.value,
             "price": revProPr.value,
             "userId": sessionStorage.getItem("userId")
         },
         "sorts_name": sortList
+    }
+
+    if (revPP) {
+        body.product_info.productPicture = revPP;
     }
 
     fetch("http://localhost:8080/revise_product", {
@@ -213,6 +265,299 @@ revProB.addEventListener("click", function() {
         const checkData = JSON.parse(JSON.stringify(data));
         if(checkData.message) {
             mge.innerHTML = "\u00A0" + `${checkData.message}` + "\u00A0";
+        }
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+})
+
+// 帶出刪除頁面選取的商品名稱
+
+const deleProId = document.querySelector("#deleProId");
+const deleProN = document.querySelector("#deleProName");
+const deleProB = document.querySelector("#deleProBtn");
+const proDeleM = document.querySelector("#proDeleModal");
+
+function proDeleCheckAlert() {
+    proDeleM.style.display = "flex";
+    
+}
+
+function closeProDeleCheAlert() {
+    proDeleM.style.display = "none";
+}
+
+deleProId.addEventListener("change", function() {
+    let body = {
+        "product_id": deleProId.value
+    }
+
+    fetch("http://localhost:8080/get_pro_info", {
+        method: "Post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        const checkData = JSON.parse(JSON.stringify(data))
+        if(checkData) {
+            deleProN.value = checkData.product_name
+        }
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+})
+
+// 刪除商品
+
+deleProB.addEventListener("click", function() {
+    let body = {
+        "product_info": {
+            "productId": deleProId.value,
+            "productName": deleProN.value,
+            "userId": sessionStorage.getItem("userId")
+        }
+    }
+
+    fetch("http://localhost:8080/delete_product", {
+        method: "Post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        const checkData = JSON.parse(JSON.stringify(data));
+        if(checkData.message) {
+            mge.innerHTML = "\u00A0" + `${checkData.message}` + "\u00A0";
+        }
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+})
+
+// 帶出上下架商品頁面選取的商品名稱
+
+const CSProId = document.querySelector("#chaStateProId");
+const chaStaProN = document.querySelector("#chaStateProName");
+const chaStaB = document.querySelector("#chaStateBtn");
+const sta = document.querySelector("#state");
+
+CSProId.addEventListener("change", function() {
+    let body = {
+        "product_id": CSProId.value
+    }
+
+    fetch("http://localhost:8080/get_pro_info", {
+        method: "Post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        const checkData = JSON.parse(JSON.stringify(data))
+        if(checkData) {
+            chaStaProN.value = checkData.product_name
+            sta.value = checkData.state
+        }
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+})
+
+// 上下架商品
+
+chaStaB.addEventListener("click", function() {
+    body = {
+        "product_info": {
+            "productId": CSProId.value,
+            "userId": sessionStorage.getItem("userId"),
+            "state": sta.value
+        }
+    }
+
+    fetch("http://localhost:8080/change_state", {
+        method: "Post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        const checkData = JSON.parse(JSON.stringify(data))
+        if(checkData.message) {
+            mge.innerHTML = "\u00A0" + `${checkData.message}` + "\u00A0";
+        }
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+})
+
+// 帶出會員原資料
+
+const na = document.querySelector("#name");
+const birth = document.querySelector("#birthDate");
+const ma = document.querySelector("#mail");
+const edInB = document.querySelector("#edInBtn");
+
+let userM = {
+    "member_info": {
+        "userId": sessionStorage.getItem("userId")
+    }
+}
+
+fetch("http://localhost:8080/find_mem_info", {
+    method: "Post",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(userM)
+})
+.then(function(response) {
+    return response.json()
+})
+.then(function(data) {
+    const checkData = JSON.parse(JSON.stringify(data))
+    if(checkData.member_info) {
+        na.value = checkData.member_info.name
+        ma.value = checkData.member_info.mail
+        birth.value = checkData.member_info.birthDate
+    }
+})
+.catch(function(error) {
+    console.log(error)
+})
+
+// 修改會員資料
+
+edInB.addEventListener("click", function() {
+    body = {
+        "member_info": {
+            "name": na.value,
+            "mail": ma.value,
+            "birthDate": birth.value,
+            "userId": sessionStorage.getItem("userId")
+        }
+    }
+    
+    fetch("http://localhost:8080/edit_mem_info", {
+        method: "Post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data) {
+        const checkData = JSON.parse(JSON.stringify(data))
+        if(checkData.message) {
+            mge.innerHTML = "\u00A0" + `${checkData.message}` + "\u00A0";
+        }
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+})
+
+// 修改密碼
+
+const pw = document.querySelector("#pwd");
+const nPw = document.querySelector("#newPwd");
+const nPwC = document.querySelector("#newPwdCheck");
+const chPwB = document.querySelector("#chPwBtn");
+
+chPwB.addEventListener("click", function() {
+    let body = {
+        "member_info": {
+            "userId": sessionStorage.getItem("userId"),
+            "password": pw.value
+        },
+        "new_pwd": nPw.value,
+        "new_pwd_check": nPwC.value
+    }
+
+    fetch("http://localhost:8080/change_pwd", {
+        method: "Post",
+        headers: {  
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(body)
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        const checkData = JSON.parse(JSON.stringify(data))
+        if(checkData.message) {
+            mge.innerHTML = "\u00A0" + `${checkData.message}` + "\u00A0";
+        }
+    })
+    .catch(function(error) {
+        console.log(error)
+    })
+})
+
+// 刪除用戶名
+
+const memDeleM = document.querySelector("#memDeleModal");
+const deleMemB = document.querySelector("#deleMemBtn");
+
+function memDeleCheckAlert() {
+    memDeleM.style.display = "flex";
+    
+}
+
+function closeMemDeleCheAlert() {
+    memDeleM.style.display = "none";
+}
+
+deleMemB.addEventListener("click", function() {
+    body = {
+        "member_info": {
+            "userId": sessionStorage.getItem("userId")
+        }
+    }
+
+    fetch("http://localhost:8080/dele_mem_info", {
+        method: "Post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        const checkData = JSON.parse(JSON.stringify(data))
+        if(checkData.message) {
+            mge.innerHTML = "\u00A0" + `${checkData.message}` + "\u00A0";
+            setTimeout(function() {
+                sessionStorage.removeItem("userId")
+                alert("跳轉至登入頁面")
+                window.location.href = "../logInPage/logInPage.html"
+            },2000)
         }
     })
     .catch(function(error) {
